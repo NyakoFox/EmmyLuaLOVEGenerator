1ab04687f66590b75e422a473dc96719e4c22030
NyakoFox <nyakowofox@gmail.com>
2025-07-03 20:07:19 -0300

    Initial changes to the Emmy-love-api script
    The changes in this repo are all documented in the instructions.

---
+++ README.md
@@ -0,0 +1,47 @@
+# EmmyLua LÖVE Generator
+
+A script to generate [LÖVE](https://love2d.org/) API autocomplete files for [EmmyLua](https://github.com/EmmyLuaLs/emmylua-analyzer-rust), based off of [Emmy-love-api](https://github.com/EmmyLua/Emmy-love-api).
+
+## Instructions
+
+1. Download this repository.
+2. Download the version of the [LÖVE API](https://github.com/love2d-community/love-api) that you want.
+3. Ensure that the `api` folder exists. If not, create it.
+4. Copy both the LÖVE API's `modules` folder and the `love_api.lua` file into this repository's root directory.
+5. Run `genEmmyAPI.lua` using your computer's installation of Lua. Ex. `lua genEmmyAPI.lua`.
+6. Copy the `api` folder into your project's source folder, the same folder where `main.lua` is (you can rename it whatever you want, it doesn't have to be called `api`).
+7. In your project, add the directory you just created into your `.emmyrc.json`, in the array in `workspace` -> `library`.
+
+Once you start or refresh your IDE (might be automatic) you should have autocomplete and quick documentation for LÖVE!
+
+## Other LÖVE versions
+
+When you want to regenerate the autocomplete files, delete the contents of the `api` folder (not the folder itself!) and redo the steps.
+
+## Changes from Emmy-love-api
+
+- Nothing included in this repository by default to avoid people using old versions of the LÖVE API
+- Optional arguments are now properly generated
+  - Marked as optional in the `---@param` defines
+  - Appends `(Defaults to <default>.)` to the description
+
+- Should now work with modern EmmyLua:
+  - Definition files now use `---@meta` rather than classes
+  - `m.function` and `return m` were removed, now relying on meta syntax
+  - All files have a namespace definition for `love`, meaning that `love.Object` is the type, which will not conflict with your own `Object` type
+
+- Added console output
+- Styling changes
+  - Replaced generated tabs with spaces
+  - Indents should now be more consistent
+  - None of the generated files should have default warnings
+
+- Small organization changes
+
+## Credits
+
+Original script by [@tangzx](https://github.com/tangzx)
+
+Modifications & original README by [@kindfulkirby](https://github.com/kindfulkirby)
+
+Updated for modern EmmyLua by [@NyakoFox](https://github.com/NyakoFox)
---
+++ genEmmyAPI.lua
@@ -0,0 +1,169 @@
+local time = os.clock()
+
+print("Requiring love_api")
+
+local api = require('love_api')
+
+local function safeDesc(src, prefix)
+    prefix = prefix or ""
+    return "--- " .. string.gsub(src, "\n", "\n" .. prefix .. "--- ")
+end
+
+local function genReturns(variant)
+    local returns = variant.returns
+    local s = ""
+    local num = 0
+    if returns and #returns > 0 then
+        num = #returns
+        for i, ret in ipairs(returns) do
+            if i == 1 then
+                s = ret.type
+            else
+                s = s .. ', ' .. ret.type
+            end
+        end
+    else
+        s = "void"
+    end
+    return s, num
+end
+
+local function genFunction(moduleName, fun, static)
+    local code = safeDesc(fun.description) .. "\n"
+    local argList = ''
+
+    for vIdx, variant in ipairs(fun.variants) do
+        -- args
+        local arguments = variant.arguments
+        if arguments and #arguments > 0 then
+            if vIdx == 1 then
+                for argIdx, argument in ipairs(arguments) do
+                    if argIdx == 1 then
+                        argList = argument.name
+                    else
+                        argList = argList .. ', ' .. argument.name
+                    end
+
+                    local type = argument.type
+                    local description = argument.description
+
+                    if (argument.default) then
+                        type = type .. "?"
+                        description = description .. " (Defaults to " .. argument.default .. ".)"
+                    end
+                    code = code .. '---@param ' .. argument.name .. ' ' .. type .. ' # ' .. description .. '\n'
+                end
+            else
+                code = code .. '---@overload fun('
+                for argIdx, argument in ipairs(arguments) do
+                    if argIdx == 1 then
+                        code = code .. argument.name .. ':' .. argument.type
+                    else
+                        code = code .. ', '
+                        code = code .. argument.name .. ':' .. argument.type
+                    end
+                end
+                code = code .. '):' .. genReturns(variant)
+                code = code .. '\n'
+            end
+        end
+
+        if vIdx == 1 then
+            local type, num = genReturns(variant)
+            if num > 0 then
+                code = code .. '---@return ' .. type .. '\n'
+            end
+        end
+    end
+
+    local dot = static and '.' or ':'
+    code = code .. "function " .. moduleName .. dot .. fun.name .. "(" .. argList .. ") end\n\n"
+    return code
+end
+
+local function genType(name, type)
+    local code = "---@class " .. type.name
+    if type.parenttype then
+        code = code .. ' : ' .. type.parenttype
+    end
+    code = code .. '\n'
+    code = code .. safeDesc(type.description) .. '\n'
+    code = code .. 'local ' .. name .. ' = {}\n'
+    -- functions
+    if type.functions then
+        for i, fun in ipairs(type.functions) do
+            code = code .. genFunction(name, fun, false)
+        end
+    end
+
+    return code
+end
+
+local function genEnum(enum)
+    local code = safeDesc(enum.description) .. '\n'
+    code = code .. enum.name .. ' = {\n'
+    for i, const in ipairs(enum.constants) do
+        code = code .. '    ' .. safeDesc(const.description, '    ') .. '\n'
+        local name = const.name
+        if name == '\\' then
+            name = '\\\\'
+        elseif name == '\'' then
+            name = '\\\''
+        end
+        code = code .. '    [\'' .. name .. '\'] = ' .. i .. ',\n'
+    end
+    code = code .. '}\n'
+    return code
+end
+
+local function genModule(name, api)
+    print("Generating module " .. name)
+    local f = assert(io.open("api/" .. name .. ".lua", 'w'))
+    f:write("---@meta\n")
+    f:write("---@namespace love\n\n")
+
+    if api.description then
+        f:write(safeDesc(api.description) .. '\n')
+    end
+
+    f:write(name .. " = {}\n\n")
+
+    -- types
+    if api.types then
+        for i, type in ipairs(api.types) do
+            f:write('--region ' .. type.name .. '\n')
+            f:write(genType(type.name, type))
+            f:write('--endregion ' .. type.name .. '\n')
+        end
+    end
+
+    -- enums
+    if api.enums then
+        for i, enum in ipairs(api.enums) do
+            f:write(genEnum(enum))
+        end
+    end
+
+    -- modules
+    if api.modules then
+        for i, m in ipairs(api.modules) do
+            f:write("---@type " .. name .. '.' .. m.name .. '\n')
+            f:write(name .. "." .. m.name .. ' = nil\n\n')
+            genModule(name .. '.' .. m.name, m)
+        end
+    end
+
+    -- functions
+    for i, fun in ipairs(api.functions) do
+        f:write(genFunction(name, fun, true))
+    end
+
+    -- Add newline at the end of the file
+    f:close()
+end
+
+genModule('love', api)
+
+local completed = os.clock() - time
+print("--------")
+print('Completed in ' .. (completed * 1000) .. 'ms.')
